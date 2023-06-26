fe3376fa751e71822727d792c93239ca3233df12
Renato de Souza <renatocdesouza@gmail.com>
2023-06-26 13:57:23 -0300

    AjusteS

--- lib/br/cpf/cpf_wrapper.rb
+++ lib/br/cpf/cpf_wrapper.rb
@@ -4,7 +4,7 @@ module BR
     extend FFI::Library
     path = File.join(File.dirname(__FILE__), "..", "..")
     lib = File.join(path, 'CPF.so')
-    lib = File.join(path, 'CPF.bundle') unless File.exists?(lib)
+    lib = File.join(path, 'CPF.bundle') unless File.exist?(lib)
     ffi_lib lib
     attach_function :calc_check_digit, [:long_long], :long_long
     attach_function :is_valid, [:long_long], :bool
